ccd45ff2329d3b727ed9f069845b9f853e94975d
Diego Marinho <diegomarinhoalmeida@gmail.com>
2024-12-24 21:46:21 -0300

    [feat][PBI-123]: add file vercel

--- src/main/java/br/com/api/device/domain/factory/DeviceFactory.java
+++ src/main/java/br/com/api/device/domain/factory/DeviceFactory.java
@@ -7,7 +7,6 @@ public abstract class DeviceFactory {
 
     public abstract Device createDevice(DeviceDTO deviceDTO);
 
-    // Método opcional, exemplo de Factory com transformação de DTO para Entity
     public static DeviceDTO transformToDTO(Device device) {
         return DeviceDTO.create(device.getId(), device.getName(), device.getBrand(), device.getCreationTime());
     }
